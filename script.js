c888e91abb57f0983a049d176c3cac4bd66884ed
Atikacelestine <celestineatika313@gmail.com>
2024-10-13 23:52:02 +0300

    Update script.js

--- script.js
+++ script.js
@@ -2,12 +2,60 @@ document.addEventListener('DOMContentLoaded', function(){
     //select the add task button and store in a constant named addButton
     const addButton= document.getElementById('add-task-btn');
 
-    //selectt the input field where users enter tasks
+    //select the input field where users enter tasks
     const taskInput= document.getElementById('task-input');
 
-    //select thhe unordered list where the tasks will be added
+    //select the unordered list where the tasks will be added
     const taskList = document.getElementById('task-list');
+
+    //intialize the task array 
+    let tasks =[];
+
+    //function to load tasks from the local storage and populate the task list
+    function loadTasks(){
+        const storedTasks= localStorae.getItem('tasks');
+        if(storedTasks){
+            tasks=JSON.parse(storedTasks);
+            tasks.forEach(function(taskText);{
+                createTaskElement(taskText);
 })
+        }
+    }
+
+    //function to create a new task
+    function createTaskElement(taskText){
+
+        const li=
+            document.createElement('li');
+        li.textContent=taskText;
+
+        //Create a new button element for removing tasks
+        const removeButton = document.createElement('button');
+        removeButton.textContent="Remove";
+        removeButton.classList.add="remove-btn";
+
+        //Assign an onclick event to the remove button 
+        removeButton.onclick=function(){
+            //remove the task from thr DOM
+            taskList.removeChild(li);
+
+            //remove the task from the tasks array
+            tasks=tasks.filter(task==>task!==taskText);
+
+            //Update local storage 
+            saveTaks();
+        };
+
+        //append te remove button to the li element
+        li.appendChild(removeButton);
+
+        //append the li element to the taskList
+        taskList.appendChild(li);
+    }
+
+    //Function to save tasks array to local storage 
+    function saveTasks(){
+        localStorage.setItem('tasks',JSON.stringify(tasks));
 function addTask(){
     //retrieve and trim the value from the task input field
     const taskText= taskInput.value.trim();
@@ -17,7 +65,26 @@ if(taskText ===""){
     alert("Please enter a task.");
     return;//exit the function
 }
+    //Add a new task to the DOM
+    createTaskElement(taskText);
+    //Add a new task to the tasks array 
+    tasks.push(taskText);
+
+    //Saved te updated tasks array to local storage 
+    savedTasks();
+    
+//Clear the task inout field 
+    taskInput.value="";
+}
+
+        addButton.addEventListener('click',addTask);
+
+        taskInput.addEventListener('keypress',function(event){
+            if(event.key==='Enter'){addTask();
+                                   }
+    
 };
+        loadTasks();
 
 //Create a new li element
 const li= document.createElement('li');
